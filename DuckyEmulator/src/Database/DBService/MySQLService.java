7e975cb7cf7db78244f0c2cddc9fd826ed95da4c
DucksaberVN <business@ducksabervn.com>
2025-01-25 02:59:52 +0700

    Create MySQLService.java

---
+++ DuckyEmulator/src/Database/DBService/MySQLService.java
@@ -0,0 +1,7 @@
+package Database.DBService;
+
+public class MySQLService {
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "123456789";
+    private static final String CONN_STRING = "jdbc:mysql://localhost/DuckyEmulator_QuestionDB";
+}
