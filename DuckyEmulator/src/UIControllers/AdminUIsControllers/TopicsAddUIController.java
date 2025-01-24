e667a759128acaf604b4bb9022c98e58029785fc
DucksaberVN <business@ducksabervn.com>
2025-01-25 03:00:38 +0700

    Create TopicsAddUIController.java

---
+++ DuckyEmulator/src/UIControllers/AdminUIsControllers/TopicsAddUIController.java
@@ -0,0 +1,33 @@
+package UIControllers.AdminUIsControllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class TopicsAddUIController {
+
+    @FXML
+    private TextArea txtAreaTopicDescription;
+
+    @FXML
+    private Button btnAddNewTopic;
+
+    @FXML
+    private Button btnResetField;
+
+    @FXML
+    private TextField txtFieldTopicName;
+
+    @FXML
+    void btnAddNewTopicClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnResetFieldClick(ActionEvent event) {
+
+    }
+
+}
