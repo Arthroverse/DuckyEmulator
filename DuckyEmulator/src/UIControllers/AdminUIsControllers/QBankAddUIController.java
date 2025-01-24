e9b6ebca494d2667b0e59d65b57306a8ae89bbcd
DucksaberVN <business@ducksabervn.com>
2025-01-24 15:51:40 +0700

    Create QBankAddUIController.java

---
+++ DuckyEmulator/src/UIControllers/AdminUIsControllers/QBankAddUIController.java
@@ -0,0 +1,63 @@
+package UIControllers.AdminUIsControllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class QBankAddUIController {
+
+    @FXML
+    private ChoiceBox<?> choiceBoxSelectTopic;
+
+    @FXML
+    private ChoiceBox<?> choiceBoxCorrectAns;
+
+    @FXML
+    private Button btnChooseImagePath;
+
+    @FXML
+    private TextField txtFieldImagePath;
+
+    @FXML
+    private ChoiceBox<?> choiceBoxSelectClass;
+
+    @FXML
+    private TextArea txtxAreaQChoice2;
+
+    @FXML
+    private TextArea txtxAreaQChoice1;
+
+    @FXML
+    private Button btnAddNewTopic;
+
+    @FXML
+    private TextArea txtAreaQStatement;
+
+    @FXML
+    private TextArea txtxAreaQChoice4;
+
+    @FXML
+    private TextArea txtxAreaQChoice3;
+
+    @FXML
+    private Button btnResetField;
+
+    @FXML
+    void btnChooseImagePathClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnAddNewTopicClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnResetFieldClick(ActionEvent event) {
+
+    }
+
+}
