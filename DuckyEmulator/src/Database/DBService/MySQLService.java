606af41724bb66bddc0634e0b01b003f6616fad4
DucksaberVN <business@ducksabervn.com>
2025-01-26 12:54:43 +0700

    Update MySQLService.java

--- DuckyEmulator/src/Database/DBService/MySQLService.java
+++ DuckyEmulator/src/Database/DBService/MySQLService.java
@@ -1,7 +1,15 @@
 package Database.DBService;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MySQLService {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "123456789";
     private static final String CONN_STRING = "jdbc:mysql://localhost/DuckyEmulator_QuestionDB";
+
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+    }
 }
