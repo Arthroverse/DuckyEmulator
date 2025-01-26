d7a4afba0de0af4b62c41b550c766e26bd922c0b
DucksaberVN <business@ducksabervn.com>
2025-01-26 12:55:59 +0700

    Update TopicsAddUIController.java

--- DuckyEmulator/src/UIControllers/AdminUIsControllers/TopicsAddUIController.java
+++ DuckyEmulator/src/UIControllers/AdminUIsControllers/TopicsAddUIController.java
@@ -1,12 +1,20 @@
 package UIControllers.AdminUIsControllers;
 
+import Database.MainDB.Beans.Topics;
+import UIs.Navigator;
+import Utilities.PromptAlert.AlertUtil;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class TopicsAddUIController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TopicsAddUIController implements Initializable {
 
     @FXML
     private TextArea txtAreaTopicDescription;
@@ -21,8 +29,13 @@ public class TopicsAddUIController {
     private TextField txtFieldTopicName;
 
     @FXML
-    void btnAddNewTopicClick(ActionEvent event) {
-
+    void btnAddNewTopicClick(ActionEvent event) throws IOException {
+        Topics t = new Topics();
+        t.setTopicName(txtFieldTopicName.getText());
+        t.setTopicDescription(txtAreaTopicDescription.getText());
+        if(inputValidation()) Topics.insert(t);
+        Navigator.getInstance().closeSecondStage();
+        Navigator.getInstance().goToTopicClassIndex();
     }
 
     @FXML
@@ -30,4 +43,19 @@ public class TopicsAddUIController {
 
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    private boolean inputValidation(){
+        StringBuilder errorMessage = new StringBuilder();
+        if(txtFieldTopicName.getText().isEmpty()) errorMessage.append("Topic name shouldn't be leave empty !\n");
+        if(!errorMessage.toString().isEmpty()){
+            AlertUtil.generateErrorWindow("Add new topic failed", "Add new topic",
+                    errorMessage.toString());
+            return false;
+        }
+        return true;
+    }
 }
