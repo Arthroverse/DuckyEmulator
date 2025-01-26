eff550f9add03f9c51f5f89a60d31c01d9c7d2e2
DucksaberVN <business@ducksabervn.com>
2025-01-26 12:55:29 +0700

    Update ClassificationsAddUIController.java

--- DuckyEmulator/src/UIControllers/AdminUIsControllers/ClassificationsAddUIController.java
+++ DuckyEmulator/src/UIControllers/AdminUIsControllers/ClassificationsAddUIController.java
@@ -1,12 +1,20 @@
 package UIControllers.AdminUIsControllers;
 
+import Database.MainDB.Beans.Classifications;
+import UIs.Navigator;
+import Utilities.PromptAlert.AlertUtil;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class ClassificationsAddUIController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClassificationsAddUIController implements Initializable {
 
     @FXML
     private TextField txtFieldClassificationName;
@@ -21,8 +29,13 @@ public class ClassificationsAddUIController {
     private Button btnAddNewClassification;
 
     @FXML
-    void btnAddNewClassificationClick(ActionEvent event) {
-
+    void btnAddNewClassificationClick(ActionEvent event) throws IOException {
+        Classifications clazz = new Classifications();
+        clazz.setClassification(txtFieldClassificationName.getText());
+        clazz.setClassificationDescription((txtAreaClassificationDescription.getText()));
+        if(inputValidation()) Classifications.insert(clazz);
+        Navigator.getInstance().closeSecondStage();
+        Navigator.getInstance().goToTopicClassIndex();
     }
 
     @FXML
@@ -30,4 +43,19 @@ public class ClassificationsAddUIController {
 
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    private boolean inputValidation(){
+        StringBuilder errorMessage = new StringBuilder();
+        if(txtFieldClassificationName.getText().isEmpty()) errorMessage.append("Classification shouldn't be leave empty !\n");
+        if(!errorMessage.toString().isEmpty()){
+            AlertUtil.generateErrorWindow("Add new classification failed", "Add new classification",
+                    errorMessage.toString());
+            return false;
+        }
+        return true;
+    }
 }
