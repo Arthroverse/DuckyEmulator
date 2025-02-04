55d641e3d01e04941f804bea48336eaf91ee70fd
DucksaberVN <business@ducksabervn.com>
2025-02-05 00:00:29 +0700

    upload proj file

--- DuckyEmulator/out/production/DuckyEmulator/Database/MainDB/MainDB.sql
+++ DuckyEmulator/out/production/DuckyEmulator/Database/MainDB/MainDB.sql
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2025 Arthroverse Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Organization: Arthroverse Laboratory
+ * Author: Vinh Dinh Mai
+ * Contact: business@arthroverse.com
+ */
 CREATE DATABASE DuckyEmulator_QuestionDB;
 
 USE DuckyEmulator_QuestionDB;
@@ -37,12 +56,5 @@ CREATE TABLE QTRelationship(
     FOREIGN KEY (TopicId) REFERENCES Topics(TopicId),
     PRIMARY KEY(QuestionId, TopicId)
 );
-<<<<<<< Updated upstream
-
-SELECT *
-FROM QTRelationship;
-
-=======
->>>>>>> Stashed changes
 -- FOR TESTING AND DEBUGGING PURPOSES
 -- DROP DATABASE DuckyEmulator_QuestionDB;
