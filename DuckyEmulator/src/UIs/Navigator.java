ed804f3b0f6cc7e900425930161f76459b9be392
DucksaberVN <business@ducksabervn.com>
2025-02-21 08:30:55 +0700

    Update Navigator.java

--- DuckyEmulator/src/UIs/Navigator.java
+++ DuckyEmulator/src/UIs/Navigator.java
@@ -1,8 +1,30 @@
+/*
+ * Copyright (c) 2025 Arthroverse Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Organization: Arthroverse Laboratory
+ * Author: Vinh Dinh Mai
+ * Contact: business@arthroverse.com
+ *
+ *
+ * @author ducksabervn
+ */
 package UIs;
 
-import Database.MainDB.Beans.Classifications;
-import Database.MainDB.Beans.Questions;
-import Database.MainDB.Beans.Topics;
+import Database.MainDB.AdminBeans.Classifications;
+import Database.MainDB.AdminBeans.Questions;
+import Database.MainDB.AdminBeans.Topics;
 import UIControllers.AdminUIsControllers.ClassificationsUpdateUIController;
 import UIControllers.AdminUIsControllers.QBankAddUIController;
 import UIControllers.AdminUIsControllers.QBankUpdateUIController;
