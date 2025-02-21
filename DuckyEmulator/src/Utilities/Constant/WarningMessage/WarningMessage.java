2604e4e16ceea2f815e6dd80bebe4689885f364b
DucksaberVN <business@ducksabervn.com>
2025-02-21 08:31:34 +0700

    Create WarningMessage.java

---
+++ DuckyEmulator/src/Utilities/Constant/WarningMessage/WarningMessage.java
@@ -0,0 +1,16 @@
+package Utilities.Constant.WarningMessage;
+
+public enum WarningMessage{
+
+    UNIVERSAL_RESET_FIELD("Are you sure you want to reset all fields ?");
+
+    private String message;
+    private WarningMessage(String message){
+        this.message = message;
+    }
+
+    @Override
+    public String toString(){
+        return this.message;
+    }
+}
