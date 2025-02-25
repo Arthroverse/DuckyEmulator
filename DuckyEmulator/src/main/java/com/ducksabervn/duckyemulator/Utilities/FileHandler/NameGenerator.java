41b0848e38074404a4498790399c08a8ef45b66f
DucksaberVN <business@ducksabervn.com>
2025-02-25 09:07:19 +0700

    Create NameGenerator.java

---
+++ DuckyEmulator/src/main/java/com/ducksabervn/duckyemulator/Utilities/FileHandler/NameGenerator.java
@@ -0,0 +1,9 @@
+package com.ducksabervn.duckyemulator.Utilities.FileHandler;
+
+import java.util.UUID;
+
+public final class NameGenerator {
+    public static String fileNameGenerator(int qId){
+        return UUID.randomUUID() + "_diagram_" + "qId" + qId;
+    }
+}
