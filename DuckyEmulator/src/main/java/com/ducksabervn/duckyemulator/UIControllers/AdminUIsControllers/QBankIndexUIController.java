2844b007d8301545e58a5c5681eb61540a9ae2a5
DucksaberVN <business@ducksabervn.com>
2025-02-25 09:04:24 +0700

    Create QBankIndexUIController.java

---
+++ DuckyEmulator/src/main/java/com/ducksabervn/duckyemulator/UIControllers/AdminUIsControllers/QBankIndexUIController.java
@@ -0,0 +1,241 @@
+/*
+ * Copyright (c) 2025 Arthroverse Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Organization: Arthroverse Laboratory
+ * Author: Vinh Dinh Mai
+ * Contact: business@arthroverse.com
+ *
+ *
+ * @author ducksabervn
+ */
+package com.ducksabervn.duckyemulator.UIControllers.AdminUIsControllers;
+
+import com.ducksabervn.duckyemulator.Database.MainDB.AdminBeans.Questions;
+import com.ducksabervn.duckyemulator.Database.MainDB.CredentialBeans.Users;
+import com.ducksabervn.duckyemulator.UIs.Navigator;
+import com.ducksabervn.duckyemulator.Utilities.Constant.ErrorMessage;
+import com.ducksabervn.duckyemulator.Utilities.Constant.ErrorTitle;
+import com.ducksabervn.duckyemulator.Utilities.Constant.Reusable;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.ducksabervn.duckyemulator.Utilities.PromptAlert.AlertUtil;
+
+public class QBankIndexUIController implements Initializable {
+
+    @FXML
+    private TableColumn<Questions, String> tableClassCol;
+
+    @FXML
+    private TableColumn<Questions, String> tableChoice4Col;
+
+    @FXML
+    private TableColumn<Questions, String> tableChoice3Col;
+
+    @FXML
+    private TableColumn<Questions, String> tableCorrectAnsCol;
+
+    @FXML
+    private Button btnMenuQuestion;
+
+    @FXML
+    private TableColumn<Questions, String> tableChoice1Col;
+
+    @FXML
+    private Button btnMenuHome;
+
+    @FXML
+    private TableColumn<Questions, String> tableChoice2Col;
+
+    @FXML
+    private TableView<Questions> tableBankView;
+
+    @FXML
+    private TableColumn<Questions, String> tableQuestStateCol;
+
+    @FXML
+    private Button btnTableDelete;
+
+    @FXML
+    private Button btnMenuTopicClass;
+
+    @FXML
+    private Button btnTableAdd;
+
+    @FXML
+    private Button btnTableUpdate;
+
+    @FXML
+    private Pagination pageinationQBank;
+
+    @FXML
+    private TableColumn<Questions, String> tableImagePath;
+
+    @FXML
+    private Label greetingLabel;
+
+    @FXML
+    private Button btnLogout;
+
+    private static int offset;
+
+    private static int maxPageNum;
+
+    private static int currentPageIndex;
+
+    public static Questions originalQuestion;
+
+    public static int getOffset(){
+        return offset;
+    }
+
+    public static int getMaxPageNum(){
+        return maxPageNum;
+    }
+
+    public static void setOffset(int offset){
+        QBankIndexUIController.offset = offset;
+    }
+
+    public static void setMaxPageNum(int maxPageNum){
+        QBankIndexUIController.maxPageNum = maxPageNum;
+    }
+
+    @FXML
+    void btnTableAddClick(ActionEvent event) throws IOException {
+        currentPageIndex = pageinationQBank.getCurrentPageIndex();
+        Navigator.getInstance().goToQBankAdd();
+    }
+
+    @FXML
+    void btnTableDeleteClick(ActionEvent event) throws IOException {
+        Questions selectedQuest = tableBankView.getSelectionModel().getSelectedItem();
+        if(AlertUtil.generateWarningWindow(
+                "Delete question confirmation",
+                "Are you sure you want to delete the selected question ?"
+        )){
+            if(Questions.delete(selectedQuest)){
+                if(tableBankView.getItems().size() == 1 & currentPageIndex > 0){
+                    currentPageIndex = pageinationQBank.getCurrentPageIndex() - 1;
+                }
+                tableBankView.getItems().remove(selectedQuest);
+                Navigator.getInstance().goToQBankIndex();
+            }
+        }
+    }
+
+    @FXML
+    void btnTableUpdateClick(ActionEvent event) throws IOException {
+        Questions selectedQuest = tableBankView.getSelectionModel().getSelectedItem();
+        if(selectedQuest == null){
+            AlertUtil.generateErrorWindow(ErrorTitle.QUEST_UI_CONTROLLER_DELETE_QUEST_FAILED.toString(),
+                    "Question deletion",
+                    ErrorMessage.QUEST_UI_CONTROLLER_DELETE_QUEST_FAILED.toString());
+        }
+        else{
+            currentPageIndex = pageinationQBank.getCurrentPageIndex();
+            Navigator.getInstance().goToQBankUpdate(selectedQuest);
+            originalQuestion = selectedQuest;
+        }
+    }
+
+    @FXML
+    void btnMenuHome(ActionEvent event) throws IOException {
+        Navigator.getInstance().goToHome();
+    }
+
+    @FXML
+    void btnMenuQuestionClick(ActionEvent event) throws IOException {
+        Navigator.getInstance().goToQBankIndex();
+    }
+
+    @FXML
+    void btnMenuTopicClassClick(ActionEvent event) throws IOException {
+        Navigator.getInstance().goToTopicClassIndex();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        greetingLabel.setText(String.format(Reusable.DEFAULT_GREETING.toString(),
+                Users.getUserNameForFrontEnd()));
+        Questions.setPage();
+        pageinationQBank.setPageCount(maxPageNum);
+        pageinationQBank.setCurrentPageIndex(currentPageIndex);
+        deploy(currentPageIndex);
+        btnTableDelete.disableProperty().set(true);
+        btnTableUpdate.disableProperty().set(true);
+        pageinationQBank.currentPageIndexProperty().addListener(
+                (observable, oldIndex, newIndex) -> {
+                    int pageIndex = newIndex.intValue();
+                    currentPageIndex = pageIndex;
+                    deploy(pageIndex);
+                });
+        tableBankView.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldSelection, newSelection) -> {
+                    if(newSelection != null){
+                        btnTableDelete.disableProperty().set(false);
+                        btnTableUpdate.disableProperty().set(false);
+                    }
+                }
+        );
+    }
+
+    @FXML
+    void btnLogoutClick(ActionEvent event) throws IOException {
+        Navigator.getInstance().goToLoginPage();
+    }
+
+    private void deploy(int pageIndex){
+        offset = pageIndex * 10;
+        tableBankView.setItems(Questions.select(offset));
+        tableClassCol.setCellValueFactory((questions) -> {
+            return questions.getValue().getForeignKeyClassificationIdForDisplayProperty();
+        });
+
+        tableQuestStateCol.setCellValueFactory((questions) -> {
+            return questions.getValue().getQuestionStatementProperty();
+        });
+
+        tableChoice1Col.setCellValueFactory((questions) -> {
+            return questions.getValue().getChoice1Property();
+        });
+
+        tableChoice2Col.setCellValueFactory((questions) -> {
+            return questions.getValue().getChoice2Property();
+        });
+
+        tableChoice3Col.setCellValueFactory((questions) -> {
+            return questions.getValue().getChoice3Property();
+        });
+
+        tableChoice4Col.setCellValueFactory((questions) -> {
+            return questions.getValue().getChoice4Property();
+        });
+
+        tableCorrectAnsCol.setCellValueFactory((questions) -> {
+            return questions.getValue().getCorrectAnswerProperty();
+        });
+
+        tableImagePath.setCellValueFactory((questions) -> {
+            return questions.getValue().getImagePathProperty();
+        });
+    }
+}
