32eb0bcafd19c51ea623c2fdc8d0244c6fc261b6
DucksaberVN <business@ducksabervn.com>
2025-05-26 01:30:50 +0700

    Update MySQLService.java

--- DuckyEmulator/src/main/java/com/arthroverse/duckyemulator/Database/DBService/MySQLService.java
+++ DuckyEmulator/src/main/java/com/arthroverse/duckyemulator/Database/DBService/MySQLService.java
@@ -24,12 +24,28 @@ package com.arthroverse.duckyemulator.Database.DBService;
 
 import java.sql.*;
 
+/**
+ * A class that merely serves as a connection to a MySQL localhost database
+ * <p>
+ * This class contains the necessary information to establish a connection
+ * to the MySQL database, such as username, password, and database target
+ *
+ * @author ducksabervn
+ * @version 0.1
+ * @since 2025-5-25
+ */
 public class MySQLService {
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "123456789";
-    private static final String CONN_STRING = "jdbc:mysql://localhost/DuckyEmulator_QuestionDB";
+    private static final String USERNAME = "root"; //connection username
+    private static final String PASSWORD = "123456789"; //connection password
+    private static final String CONN_STRING = "jdbc:mysql://localhost/DuckyEmulator_QuestionDB"; //database target to connect
 
+    /**
+     * This method is used to establish a connection with a database
+     *
+     * @return {@code java.sql.Connection}
+     * @throws SQLException
+     */
     public static Connection getConnection() throws SQLException{
-        return DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+        return DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD); //idk what this is, this is part of java convention
     }
 }
