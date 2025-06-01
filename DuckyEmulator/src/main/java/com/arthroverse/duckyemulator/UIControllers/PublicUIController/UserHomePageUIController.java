d71d3e4c71ac0220697ef123b0c6997ab711267d
DucksaberVN <business@ducksabervn.com>
2025-06-01 23:44:09 +0700

    Create UserHomePageUIController.java

---
+++ DuckyEmulator/src/main/java/com/arthroverse/duckyemulator/UIControllers/PublicUIController/UserHomePageUIController.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2025 Arthroverse Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Organization: Arthroverse Laboratory
+ * Author: Vinh Dinh Mai
+ * Contact: business@arthroverse.com
+ *
+ * @author ducksabervn
+ */
+package com.arthroverse.duckyemulator.UIControllers.PublicUIController;
+
+import com.arthroverse.duckyemulator.Database.MainDB.CredentialBeans.Users;
+import com.arthroverse.duckyemulator.Database.MainDB.PublicBeans.Sessions;
+import com.arthroverse.duckyemulator.UIs.Navigator;
+import com.arthroverse.duckyemulator.Utilities.Constant.Reusable;
+import com.arthroverse.duckyemulator.Utilities.Constant.WarningMessage;
+import com.arthroverse.duckyemulator.Utilities.Constant.WarningTitle;
+import com.arthroverse.duckyemulator.Utilities.PromptAlert.AlertUtil;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
+
+/**
+ * Controller class for UserHomePage.fxml
+ * Contains field declarations for FXML elements that would have fx:id attributes
+ *
+ * Note: Currently the FXML file has no fx:id attributes.
+ * Add fx:id="fieldName" to FXML elements to connect them to these fields.
+ */
+public class UserHomePageUIController implements Initializable {
+    @FXML
+    private Label greetingLabel;
+
+    private static LocalDateTime startTime;
+
+    public static LocalDateTime getStartTime(){
+        return startTime;
+    }
+
+    @FXML
+    public void btnStartSession(ActionEvent event) throws IOException {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
+        Sessions session = new Sessions();
+        session.setSessionId(now.format(formatter));
+        Sessions.insert(session);
+        if(AlertUtil.generateWarningWindow(WarningTitle.USER_AGREEMENT.toString(),
+                WarningMessage.USER_AGREEMENT.toString())){
+            startTime = LocalDateTime.now();
+            Navigator.getInstance().goToTestPage();
+        }
+    }
+
+    @FXML
+    public void btnGoToHome(ActionEvent event) throws IOException {
+        Navigator.getInstance().goToUserHomePage();
+    }
+
+    @FXML
+    public void btnLogout(ActionEvent event) throws IOException {
+        Navigator.getInstance().goToLoginPage();
+    }
+
+    @FXML
+    public void btnGoToTestHistory(ActionEvent event){
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        greetingLabel.setText(String.format(Reusable.DEFAULT_GREETING.toString(),
+                Users.getUserName()));
+    }
+}
