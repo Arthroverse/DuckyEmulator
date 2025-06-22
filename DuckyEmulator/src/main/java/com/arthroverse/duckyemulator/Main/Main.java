c70727ba5a973b3dfcb6890d9cc2a171252241e1
DucksaberVN <business@ducksabervn.com>
2025-06-23 01:08:05 +0700

    Create Main.java

---
+++ DuckyEmulator/src/main/java/com/arthroverse/duckyemulator/Main/Main.java
@@ -0,0 +1,7 @@
+package com.arthroverse.duckyemulator.Main;
+
+public class Main {
+    public static void main(String[] args) {
+        DuckyEmulatorMain.main(args); //bro ts is merely serve as a solution for Maven to properly run my project as a Jar executable
+    }
+}
